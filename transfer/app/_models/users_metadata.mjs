765d3754e41166a53e78bbf6afef5cfffb66a36e
UsaidMalik <umalik2003@gmail.com>
2024-04-27 00:46:03 -0400

    added in mjs types

--- transfer/app/_models/users_metadata.mjs
+++ transfer/app/_models/users_metadata.mjs
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
-
+import { ObjectId } from "mongodb";
+import { string } from "zod";
   // the metaData associated with a user
   // that users id
   // their points that theyve earned
   // their current streak
   // and the friends they interact with
   const MetaData = new mongoose.Schema({
-    userId: {type: String, required: true},
+    userId: {type: ObjectId, required: true},
     score: {type: Number, default: 0, required: true},
     streak: {type: Number, default: 0, required: true},
     friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'List'}],
+    languages : {type: Object, default: {}, required: true}
   });
 
 export default mongoose.Model("User-Metadata", MetaData)
