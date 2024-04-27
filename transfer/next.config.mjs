45393b07668b865a0e1954ee36ff663d538180bd
UsaidMalik <umalik2003@gmail.com>
2024-04-27 00:07:14 -0400

    turned on dangerous build

--- transfer/next.config.mjs
+++ transfer/next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
 	webpack: (config) => {
 		config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
 		return config;
-	}
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	  },
 };
 
 export default nextConfig;
